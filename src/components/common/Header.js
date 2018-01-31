528bad1266d2378252711e59d334e463f62902dd
Mohamed Nor Faiz Shahudin <faiz.shahudin@prestariang.com.my>
2018-01-31 12:15:58 +0800

    Initial Commit

---
+++ src/components/common/Header.js
@@ -0,0 +1,33 @@
+// Import libraries for making a component
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+// Make a component
+const Header = (props) => {
+    return (
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}>{props.headerText}</Text>
+        </View>  
+    );   
+};
+// Make the component available to other parts of the app
+export { Header };
+
+//styling
+const styles = StyleSheet.create({
+    viewStyle: {
+        backgroundColor: '#DDDDDD',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+});
